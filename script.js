c4d1f7e5d200a0444762f369fa2571fdd5209893
Shariq Shaikh <shariqshaikh2705@gmail.com>
2025-05-10 22:37:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 //your code here
+async function fetchURL(URL){
+	let response = await fetch(URL);
+	let data = await response.json();
+}
 function findMajority(arr){
 	let half = Math.floor(arr.length/2);
 	for(let i=0;i<arr.length;i++){
@@ -14,3 +18,11 @@ function findMajority(arr){
 	}
 	
 }
+// console.log(body); 
+let array = [];
+fetchURL("http://localhost:3000/majorityElementChecker").then(
+	(data)=>{
+		array = data.arr;
+	}
+)
+findMajority(array);
