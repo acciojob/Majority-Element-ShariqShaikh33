c28f65536c30f0fce0d91a33c4e89b1e9ef35090
Shariq Shaikh <shariqshaikh2705@gmail.com>
2025-05-10 22:24:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your code here
+function findMajority(arr){
+	let half = Math.floor(arr.length/2);
+	for(let i=0;i<arr.length;i++){
+		let count = 0;
+		for(let y=i;y<arr.length;y++){
+			if(arr[y]==arr[i]){
+				count++;
+			}
+		}
+		if(count>half){
+			return arr[i];
+		}
+	}
+	
+}
